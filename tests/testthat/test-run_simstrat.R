fd3476c719423b3f5ecf894232c575aba380e307
Tadhg Moore <tadhgm@vt.edu>
2021-05-14 13:34:35 -0400

    Added tests & GH actions

---
+++ tests/testthat/test-run_simstrat.R
@@ -0,0 +1,21 @@
+context("run example simulation")
+
+test_that("running Simstrat simulation", {
+  sim_folder <- system.file('extdata', package = 'SimstratR')
+  status = run_simstrat(sim_folder, verbose = TRUE)
+  
+  expect_true(is.character(status))
+  
+  expect_true(file.exists(file.path(sim_folder, 'Results/T_out.dat')))
+  
+})
+
+test_that("running glm simulation - verbose = FALSE", {
+  sim_folder <- system.file('extdata', package = 'SimstratR')
+  status = run_simstrat(sim_folder, verbose = FALSE)
+  
+  expect_equivalent(status, 0)
+  
+  expect_true(file.exists(file.path(sim_folder, 'Results/T_out.dat')))
+  
+})
