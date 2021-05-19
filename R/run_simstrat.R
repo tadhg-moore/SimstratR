9318043eed3e9f9da79fbcf064addcba6e2d1c63
Tadhg Moore <tadhgm@vt.edu>
2021-05-19 16:59:34 -0400

    Working on mac attempt 1

--- R/run_simstrat.R
+++ R/run_simstrat.R
@@ -21,12 +21,12 @@ run_simstrat <- function (sim_folder = ".", par_file = "simstrat.par", verbose =
   if (.Platform$pkgType == "win.binary") {
     return(run_simstratWin(sim_folder, par_file, verbose))
   }
-  
+
   ### UNIX
   if (.Platform$pkgType == "source") {
     return(run_simstratNIX(sim_folder, par_file, verbose))
   }
-  
+
 
   ### macOS ###
   if (grepl('mac.binary',.Platform$pkgType)) {
@@ -43,13 +43,13 @@ run_simstrat <- function (sim_folder = ".", par_file = "simstrat.par", verbose =
 }
 
 run_simstratWin <- function(sim_folder,par_file="simstrat.par",verbose=TRUE){
-  
+
   if(.Platform$r_arch == 'x64'){
     simstrat_path <- system.file('extbin/win/simstrat.exe', package = packageName())
   }else{
     stop('No Simstrat executable available for your machine yet...')
   }
-  
+
   origin <- getwd()
   setwd(sim_folder)
 
@@ -81,7 +81,7 @@ run_simstratWin <- function(sim_folder,par_file="simstrat.par",verbose=TRUE){
 #
 #   origin <- getwd()
 #   setwd(sim_folder)
-#   
+#
 #   tryCatch({
 #     if (verbose){
 #       out <- system2(simstrat_path, wait = TRUE, stdout = "",
@@ -104,13 +104,13 @@ run_simstratWin <- function(sim_folder,par_file="simstrat.par",verbose=TRUE){
 run_simstratNIX <- function(sim_folder, par_file = 'simstrat.par', verbose=TRUE){
   simstrat_path <- system.file('exec/nixsimstrat', package = packageName())
 
-  dylib_path <- paste(system.file('extbin/nix', 
-                                  package = packageName()), 
-                      Sys.getenv('DYLD_LIBRARY_PATH'), 
+  dylib_path <- paste(system.file('extbin/nix',
+                                  package = packageName()),
+                      Sys.getenv('DYLD_LIBRARY_PATH'),
                       sep = ":")
   origin <- getwd()
   setwd(sim_folder)
-  
+
   tryCatch({
     if (verbose){
       out <- system2(simstrat_path, wait = TRUE, stdout = TRUE,
@@ -130,50 +130,67 @@ run_simstratNIX <- function(sim_folder, par_file = 'simstrat.par', verbose=TRUE)
 
 run_simstratOSx <- function(sim_folder, par_file = 'simstrat.par', verbose=TRUE){
   simstrat_path <- system.file('exec/simstrat', package= packageName())
-  
-  
+
   origin <- getwd()
   setwd(sim_folder)
-  
+
+  dylib_path <- system.file("exec", package = packageName())
   tryCatch({
     if (verbose){
-      out <- system2(simstrat_path, wait = TRUE, stdout = TRUE,
-                     stderr = "", args=par_file)
+      out <- system2(simstrat_path, wait = TRUE, stdout = "",
+                     stderr = "", args = system.args, env = paste0("DYLD_LIBRARY_PATH=", dylib_path))
     } else {
-      out <- system2(simstrat_path, stdout = NULL,
-                     stderr =NULL, args=par_file)
+      out <- system2(simstrat_path, wait = TRUE, stdout = NULL,
+                     stderr = NULL, args = system.args, env = paste0("DYLD_LIBRARY_PATH=", dylib_path))
     }
-    setwd(origin)
-    return(out)
   }, error = function(err) {
-    print(paste("Simstrat_ERROR:  ",err))
+    print(paste("SIMSTRAT_ERROR:  ",err))
   }, finally = {
     setwd(origin)
+    return(out)
   })
 }
-# 
+#
 # ### From GLEON/gotm3r
 simstrat.systemcall <- function(sim_folder, simstrat_path, verbose, system.args) {
   origin <- getwd()
   setwd(sim_folder)
 
-  tryCatch({
-    if (verbose){
-      out <- system2(simstrat_path, wait = TRUE, stdout = "",
-                     stderr = "", args = system.args)
-    } else {
-      out <- system2(simstrat_path, wait = TRUE, stdout = NULL,
-                     stderr = NULL, args = system.args)
-    }
-    setwd(origin)
-    return(out)
-  }, error = function(err) {
-    print(paste("simstrat_ERROR:  ",err))
-  }, finally = {
-    setwd(origin)
-  })
+  ### macOS ###
+  if (grepl("mac.binary",.Platform$pkgType)) {
+    dylib_path <- system.file("exec", package = packageName())
+    tryCatch({
+      if (verbose){
+        out <- system2(simstrat_path, wait = TRUE, stdout = "",
+                       stderr = "", args = system.args, env = paste0("DYLD_LIBRARY_PATH=", dylib_path))
+      } else {
+        out <- system2(simstrat_path, wait = TRUE, stdout = NULL,
+                       stderr = NULL, args = system.args, env = paste0("DYLD_LIBRARY_PATH=", dylib_path))
+      }
+    }, error = function(err) {
+      print(paste("SIMSTRAT_ERROR:  ",err))
+    }, finally = {
+      setwd(origin)
+      return(out)
+    })
+  } else {
+    tryCatch({
+      if (verbose){
+        out <- system2(simstrat_path, wait = TRUE, stdout = "",
+                       stderr = "", args = system.args)
+      } else {
+        out <- system2(simstrat_path, wait = TRUE, stdout = NULL,
+                       stderr = NULL, args = system.args)
+      }
+    }, error = function(err) {
+      print(paste("SIMSTRAT_ERROR:  ",err))
+    }, finally = {
+      setwd(origin)
+      return(out)
+    })
+  }
 }
-# 
+#
 # ### macOS ###
 # run_gotmOSx <- function(sim_folder, verbose, system.args){
 #   gotm_path <- system.file('exec/macgotm', package = 'GOTMr')
